31e633ff8771e34c85ee9d276d2abe509ba23508
YuanBensheng <yuanbensheng18@gmail.com>
2026-06-16 22:33:47 +0800

    Update get-video-key.js

--- api/get-video-key.js
+++ api/get-video-key.js
@@ -5,7 +5,7 @@ export default function handler(req, res) {
     if (!referer.includes('heluo.pro')) {
         return res.status(403).send('Forbidden');
     }
-    const KEY = 'a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6'; // 替换成你用ffmpeg生成的32位密钥
+    const KEY = '82a72e8d3160e9b5fd03ed452574a6c0'; // 替换成你用ffmpeg生成的32位密钥
     res.setHeader('Content-Type', 'application/octet-stream');
     res.setHeader('Cache-Control', 'no-store');
     res.send(Buffer.from(KEY, 'hex'));
